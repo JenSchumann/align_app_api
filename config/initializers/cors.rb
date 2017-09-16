72bd5588520195c243b9b46791f6690efd9f67bc
Jen Schumann <jenniferschumann1@gmail.com>
2017-09-16 09:54:06 -0600

    updated CORS for front/backend comm capability

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:4040'
+    origins 'localhost:4060'
 
     resource '*',
       headers: :any,
