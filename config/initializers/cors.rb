35245e94a9c07fc4b2ab880b3ac4a7c60c11f6b8
Jen Schumann <jenniferschumann1@gmail.com>
2017-09-21 14:34:18 -0600

    added frontend heroku url and updated config vars in heroku alignapi settings

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:4060'
+    origins ['align2create.herokuapp.com','localhost:4060']
 
     resource '*',
       headers: :any,
